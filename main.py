eef8b0be1c40596d2e880d5929687e721073a090
Issah45 <90643234+Issah45@users.noreply.github.com>
2023-06-16 18:51:23 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-version = (1, 0, 1)
+version = (1, 0, 3)
 
 print(f"\nIssOS {version}")
 print("By: Issah Abeebllahi Issah")
--- os.py
+++ os.py
@@ -1,6 +1,6 @@
 import sys
 
-version = (1, 0, 1)
+version = (1, 0, 3)
 
 print("\nWelcome to Iss OS!")
 
